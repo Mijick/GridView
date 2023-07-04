d451e82743f3ee98a01c21abe4e2f75c91825324
Tomasz Kurylik <fulcrumone@icloud.com>
2023-07-04 14:54:51 +0200

    1

--- Sources/GridScrollView/GridView.swift
+++ Sources/GridScrollView/GridView.swift
@@ -15,16 +15,12 @@ public struct GridView: View {
     var horizontalPadding: CGFloat = 8
     var numberOfItems: Int = 24
     var numberOfColumns: Int = 3
-
-    @State var maxu: CGFloat = 0
+    @State private var contentHeight: CGFloat = 0
+    @State private var itemHeights: [Int: CGFloat] = [:]
 
 
     public init() {}
     public var body: some View {
-        var height: [Int: CGFloat] = [:]
-
-
-
         ScrollView {
             GeometryReader { reader in
                 ZStack(alignment: .topLeading) {
@@ -32,17 +28,17 @@ public struct GridView: View {
                         createItem(index, .random, .random(index))
 
                             .alignmentGuide(.top) { d in
-                                height.updateValue(d.height, forKey: index)
+                                updateItemHeights(index, d.height)
 
 
-                                DispatchQueue.main.async { maxu = max(
-                                    maxu,
-                                    abs(d[VerticalAlignment.top] - calculateUpToIndex(height, index)) + d.height
+                                DispatchQueue.main.async { contentHeight = max(
+                                    contentHeight,
+                                    abs(d[VerticalAlignment.top] - calculateUpToIndex(itemHeights, index)) + d.height
                                 ) }
 
 
 
-                                return d[VerticalAlignment.top] - calculateUpToIndex(height, index) - CGFloat(index / numberOfColumns) * verticalPadding
+                                return d[VerticalAlignment.top] - calculateUpToIndex(itemHeights, index) - CGFloat(index / numberOfColumns) * verticalPadding
                             }
                             .alignmentGuide(.leading) { d in
 
@@ -63,12 +59,43 @@ public struct GridView: View {
             .padding(.horizontal, 28)
 
             // trzeba dodać verticalPadding
-            .frame(height: maxu)
+            .frame(height: contentHeight)
         }
     }
 }
 
 private extension GridView {
+    func updateItemHeights(_ index: Int, _ height: CGFloat) { DispatchQueue.main.async {
+        itemHeights.updateValue(height, forKey: index)
+    }}
+    func updateContentHeight(_ index: Int) { DispatchQueue.main.async {
+
+
+
+
+    }}
+}
+
+private extension GridView {
+    func getRowNumber(_ index: Int) -> Int { index / numberOfColumns }
+    func getVerticalPaddingValue(_ index: Int) -> CGFloat {
+        let rowNumber = getRowNumber(index).floatValue
+        return rowNumber * verticalPadding
+    }
+
+
+
+
+
+    //func updateContentHeight(_)
+
+
+
+
+
+
+
+
     func calculateItemWidth(_ screenWidth: CGFloat) -> CGFloat {
         let numberOfColumns = CGFloat(numberOfColumns)
         let b = (numberOfColumns - 1) * horizontalPadding
@@ -109,9 +136,62 @@ private extension GridView {
 
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+fileprivate extension Color {
+    static let random: Color = .init(.init(red: .random(in: 0...1), green: .random(in: 0...1), blue: .random(in: 0...1), alpha: 1))
+}
+fileprivate extension CGFloat {
+    static func random(_ index: Int) -> CGFloat { [100, 200, 100][index % 3 == 0 ? 0 : index % 3 == 1 ? 1 : 2] }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 private extension GridView {
+    func createScrollView() -> some View {
+        ScrollView(content: createContent)
+    }
+}
+
+private extension GridView {
+    func createContent() -> some View { GeometryReader { reader in
+        ZStack(alignment: .topLeading) {
+            //ForEach(0..<numberOfItems, id: \.self)
+        }
+    }}
+}
+
+private extension GridView {
+    func createItem(_ index: Int, _ contentSize: CGSize) {
+
+    }
 
 }
+
+
+
+
+
 
 
 
@@ -129,12 +209,7 @@ private extension GridView {
 
 
 
-
-
-
-fileprivate extension Color {
-    static let random: Color = .init(.init(red: .random(in: 0...1), green: .random(in: 0...1), blue: .random(in: 0...1), alpha: 1))
-}
-fileprivate extension CGFloat {
-    static func random(_ index: Int) -> CGFloat { [100, 200, 100][index % 3 == 0 ? 0 : index % 3 == 1 ? 1 : 2] }
+extension Int {
+    var doubleValue: Double { Double(self) }
+    var floatValue: CGFloat { CGFloat(self) }
 }
