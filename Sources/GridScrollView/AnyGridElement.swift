7c3a55f7a4b49cdbb275a7e07f43fdd018a08793
Tomasz Kurylik <fulcrumone@icloud.com>
2023-07-06 11:45:40 +0200

    1

---
+++ Sources/GridScrollView/AnyGridElement.swift
@@ -0,0 +1,23 @@
+//
+//  AnyGridElement.swift of GridScrollView
+//
+//  Created by Tomasz Kurylik
+//    - Twitter: https://twitter.com/tkurylik
+//    - Mail: tomasz.kurylik@mijick.com
+//
+//  Copyright ©2023 Mijick. Licensed under MIT License.
+
+
+import SwiftUI
+
+struct AnyGridElement: GridElement {
+    let columns: Int
+    private var _body: AnyView
+
+
+    var body: some View { _body }
+    init(_ element: some GridElement) {
+        self.columns = element.columns
+        self._body = AnyView(element.body)
+    }
+}
---
+++ Sources/GridScrollView/GridElement.swift
@@ -0,0 +1,18 @@
+//
+//  GridElement.swift of GridScrollView
+//
+//  Created by Tomasz Kurylik
+//    - Twitter: https://twitter.com/tkurylik
+//    - Mail: tomasz.kurylik@mijick.com
+//
+//  Copyright ©2023 Mijick. Licensed under MIT License.
+
+
+import SwiftUI
+
+public protocol GridElement: View {
+    var columns: Int { get }
+}
+public extension GridElement {
+    var columns: Int { 0 }
+}
