1bc5d40a2e332712cda55d465a535f929732cbf6
Tomasz Kurylik <fulcrumone@icloud.com>
2023-07-27 20:27:59 +0200

    1

--- Sources/GridScrollView/GridView.swift
+++ Sources/GridScrollView/GridView.swift
@@ -42,53 +42,40 @@ private extension GridView {
     }
 }
 
-// MARK: - Alignment Guides
+// MARK: - Vertical Alignment
 private extension GridView {
     func handleTopAlignmentGuide(_ index: Int, _ dimensions: ViewDimensions, _ reader: GeometryProxy) -> CGFloat {
         insertItem(index, dimensions.height)
 
-        let range = matrix.getRange(for: index)
-
-
-        if range.start.row == 0 {
-            return 0
-        }
-
-
-
-
-
-
-        let topPadding = -(matrix.getHeights()[range.start.row - 1][range.columns].max() ?? 0)
-
-
+        let topPadding = getTopPaddingValue(index)
         return topPadding
     }
+}
+private extension GridView {
+    func insertItem(_ index: Int, _ value: CGFloat) { DispatchQueue.main.async {
+        let item = Matrix.Item(index: index, value: value, columns: elements[index].columns)
+        matrix.insert(item)
+    }}
+    func getTopPaddingValue(_ index: Int) -> CGFloat {
+        let range = matrix.getRange(for: index)
+
+        guard range.start.row > 0 else { return 0 }
+
+        let heights = matrix.getHeights()[range.start.row - 1]
+        let itemTopPadding = heights[range.columns].max() ?? 0
+        return -itemTopPadding
+    }
+}
+
+// MARK: - Horizontal Alignment
+private extension GridView {
     func handleLeadingAlignmentGuide(_ index: Int, _ dimensions: ViewDimensions, _ reader: GeometryProxy) -> CGFloat {
         let availableWidth = reader.size.width
         let itemPadding = calculateItemLeadingPadding(index, availableWidth)
         return itemPadding
     }
 }
-
-// MARK: - Vertical Values
 private extension GridView {
-    func insertItem(_ index: Int, _ value: CGFloat) { DispatchQueue.main.async {
-        let item = Matrix.Item(index: index, value: value, columns: elements[index].columns)
-        matrix.insert(item)
-    }}
-}
-
-// MARK: - Horizontal Values
-private extension GridView {
-    func calculateItemWidth(_ index: Int, _ availableWidth: CGFloat) -> CGFloat {
-        let itemColumns = elements[index].columns
-        let singleColumnWidth = calculateSingleColumnWidth(availableWidth)
-
-        let fixedItemWidth = singleColumnWidth * itemColumns.floatValue
-        let additionalHorizontalSpacing = (itemColumns.floatValue - 1) * horizontalSpacing
-        return fixedItemWidth + additionalHorizontalSpacing
-    }
     func calculateItemLeadingPadding(_ index: Int, _ availableWidth: CGFloat) -> CGFloat {
         let columnNumber = matrix.getRange(for: index).start.column
         let singleColumnWidth = calculateSingleColumnWidth(availableWidth)
@@ -100,6 +87,19 @@ private extension GridView {
         return -rawPaddingValue
     }
 }
+
+// MARK: - Dimensions
+private extension GridView {
+    func calculateItemWidth(_ index: Int, _ availableWidth: CGFloat) -> CGFloat {
+        let itemColumns = elements[index].columns
+        let singleColumnWidth = calculateSingleColumnWidth(availableWidth)
+
+        let fixedItemWidth = singleColumnWidth * itemColumns.floatValue
+        let additionalHorizontalSpacing = (itemColumns.floatValue - 1) * horizontalSpacing
+        return fixedItemWidth + additionalHorizontalSpacing
+    }
+    func calculateContentHeight() -> CGFloat { matrix.getHeights().flatMap { $0 }.max() ?? 0 }
+}
 private extension GridView {
     func calculateSingleColumnWidth(_ availableWidth: CGFloat) -> CGFloat {
         let totalSpacingValue = getHorizontalSpacingTotalValue()
@@ -110,9 +110,4 @@ private extension GridView {
         let numberOfSpaces = matrix.numberOfColumns - 1
         return numberOfSpaces.floatValue * horizontalSpacing
     }
-}
-
-// MARK: - Calculating Content Height
-private extension GridView {
-    func calculateContentHeight() -> CGFloat { matrix.getHeights().flatMap { $0 }.max() ?? 0 }
 }
