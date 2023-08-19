0a08a6de09d90cbd3e1d4ce26238e27becf0a1f2
Tomasz Kurylik <fulcrumone@icloud.com>
2023-08-19 20:20:39 +0200

    1

--- Sources/GridScrollView/Matrix.swift
+++ Sources/GridScrollView/Matrix.swift
@@ -26,31 +26,70 @@ extension Matrix {
         let position = findPositionForItem(item)
         let range = position.createItemRange(item)
 
-
-
-
-
-
-
-
-
-
         addNewRowIfNeeded(position)
         insertItem(item, range)
 
 
 
         if isLast {
-            a()
+            sortMatrix()
             matrixInitialised = true
         }
     }}
+}
 
+// MARK: Finding Position
+private extension Matrix {
+    func findPositionForItem(_ item: Item) -> Position {
+        guard item.index > 0 else { return .zero }
 
+        let previousItemRangeEnd = getRange(for: item.index - 1).end
+        return canInsertItemInRow(item, previousItemRangeEnd) ?
+            previousItemRangeEnd.nextColumn() : previousItemRangeEnd.nextRow().withColumn(0)
+    }
+}
+private extension Matrix {
+    func canInsertItemInRow(_ item: Item, _ previousItemRangeEnd: Position) -> Bool { item.columns + previousItemRangeEnd.column < numberOfColumns }
+}
 
+// MARK: Inserting New Row
+private extension Matrix {
+    mutating func addNewRowIfNeeded(_ position: Position) { if position.row >= items.count {
+        items.insertEmptyRow(numberOfColumns: numberOfColumns)
+    }}
+}
 
+// MARK: Filling Array
+private extension Matrix {
+    mutating func insertItem(_ item: Item, _ range: Range) {
+        let columnHeight = getHeights()
 
-    private mutating func a() {
+        fillInMatrixWithEmptySpaces(range, columnHeight)
+        fillInMatrixWithItem(item, range)
+    }
+}
+private extension Matrix {
+    mutating func fillInMatrixWithEmptySpaces(_ range: Range, _ columnHeights: [[CGFloat]]) { if range.start.row > 0 {
+        range.columns.forEach { column in
+            let currentPosition = range.start.withColumn(column), previousPosition = currentPosition.previousRow()
+            let difference = columnHeights[currentPosition] - columnHeights[previousPosition]
+
+            if shouldFillInEmptySpace(difference, items[previousPosition]) {
+                items[previousPosition] = .init(index: -1, value: difference, columns: 1)
+            }
+        }
+    }}
+    mutating func fillInMatrixWithItem(_ item: Item, _ range: Range) { range.columns.forEach { column in
+        items[range.start.withColumn(column)] = item
+    }}
+}
+private extension Matrix {
+    func shouldFillInEmptySpace(_ difference: CGFloat, _ item: Item) -> Bool { difference > 0 && item.isEmpty }
+}
+
+// MARK: Sorting Matrix
+private extension Matrix {
+    mutating func sortMatrix() {
         guard policy == .fill else { return }
 
 
@@ -90,9 +129,6 @@ extension Matrix {
         }
 
 
-        //array.printujKurwa()
-
-
 
         self.items = .init(numberOfColumns: numberOfColumns)
 
@@ -111,7 +147,8 @@ extension Matrix {
             }
         }
     }
-
+}
+private extension Matrix {
     func getUniqueItems() -> [Item] {
         let nonEmptyItems = items
             .flatMap { $0 }
@@ -120,68 +157,6 @@ extension Matrix {
 
         let sortedItems = items.sorted(by: { $0.columns > $1.columns })
         return sortedItems
-    }
-
-
-
-
-
-
-}
-private extension Matrix {
-    func findPositionForItem(_ item: Item) -> Position {
-        switch policy {
-            case .ordered: return findPositionForOrderedPolicy(item)
-            case .fill: return findPositionForFillPolicy(item)
-        }
-    }
-    mutating func addNewRowIfNeeded(_ position: Position) { if position.row >= items.count {
-        items.insertEmptyRow(numberOfColumns: numberOfColumns)
-    }}
-    mutating func insertItem(_ item: Item, _ range: Range) {
-        let columnHeight = getHeights()
-
-        fillInMatrixWithEmptySpaces(range, columnHeight)
-        fillInMatrixWithItem(item, range)
-    }
-}
-// MARK: Filling Array
-private extension Matrix {
-    mutating func fillInMatrixWithEmptySpaces(_ range: Range, _ columnHeights: [[CGFloat]]) { if range.start.row > 0 {
-        range.columns.forEach { column in
-            let currentPosition = range.start.withColumn(column), previousPosition = currentPosition.previousRow()
-            let difference = columnHeights[currentPosition] - columnHeights[previousPosition]
-
-            if shouldFillInEmptySpace(difference, items[previousPosition]) {
-                items[previousPosition] = .init(index: -1, value: difference, columns: 1)
-            }
-        }
-    }}
-    mutating func fillInMatrixWithItem(_ item: Item, _ range: Range) { range.columns.forEach { column in
-        items[range.start.withColumn(column)] = item
-    }}
-}
-private extension Matrix {
-    func shouldFillInEmptySpace(_ difference: CGFloat, _ item: Item) -> Bool { difference > 0 && item.isEmpty }
-}
-// MARK: Finding Positions
-private extension Matrix {
-    func findPositionForOrderedPolicy(_ item: Item) -> Position {
-        guard item.index > 0 else { return .zero }
-
-        let previousItemRangeEnd = getRange(for: item.index - 1).end
-        return canInsertItemInRow(item, previousItemRangeEnd) ?
-            previousItemRangeEnd.nextColumn() : previousItemRangeEnd.nextRow().withColumn(0)
-    }
-    func canInsertItemInRow(_ item: Item, _ previousItemRangeEnd: Position) -> Bool { item.columns + previousItemRangeEnd.column < numberOfColumns }
-}
-private extension Matrix {
-    func findPositionForFillPolicy(_ item: Item) -> Position {
-        let columnsHeight = getHeights()[items.count - 1]
-
-        let columnIndex = columnsHeight.enumerated().min(by: { $0.element < $1.element })?.offset ?? 0
-        let rowIndex = items.firstIndex(where: { $0[columnIndex].isEmpty }) ?? items.count
-        return .init(row: rowIndex, column: columnIndex)
     }
 }
 
