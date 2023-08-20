c3e1ce4bb47b1c7aeca74cc2c4e9858bbdffce6e
Tomasz Kurylik <fulcrumone@icloud.com>
2023-08-20 14:24:37 +0200

    1

--- Sources/GridScrollView/Array++.swift
+++ Sources/GridScrollView/Array++.swift
@@ -14,21 +14,3 @@ import SwiftUI
 extension Array where Element: Hashable {
     func removingDuplicates() -> Self { Array(Set(self)) }
 }
-
-// MARK: - Testing Helpers
-extension [[CGFloat]] {
-    func print() {
-        Swift.print("\n\n")
-
-        for row in 0..<count {
-            var text = ""
-            for column in 0..<self[row].count {
-                text += "\(self[row][column])  "
-
-            }
-            Swift.print(text)
-        }
-
-        Swift.print("\n\n")
-    }
-}
---
+++ Tests/GridScrollViewTests/Helpers/Array++.swift
@@ -0,0 +1,28 @@
+//
+//  Array++.swift of GridScrollView
+//
+//  Created by Tomasz Kurylik
+//    - Twitter: https://twitter.com/tkurylik
+//    - Mail: tomasz.kurylik@mijick.com
+//
+//  Copyright ©2023 Mijick. Licensed under MIT License.
+
+
+import SwiftUI
+
+extension [[CGFloat]] {
+    func toString() -> String {
+        var text = ""
+
+        for row in 0..<count {
+            for column in 0..<self[row].count {
+                let value = Int(self[row][column])
+                text += "\(value)  "
+            }
+            text += "\n"
+        }
+
+        text.removeLast(2)
+        return text
+    }
+}
