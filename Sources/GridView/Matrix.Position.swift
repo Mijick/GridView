848d90d755ff9d6003585f0fabc210fa7aec5e88
Tomasz Kurylik <fulcrumone@icloud.com>
2023-08-20 22:17:15 +0200

    1

--- Sources/GridView/Matrix.Position.swift
+++ Sources/GridView/Matrix.Position.swift
@@ -4,6 +4,7 @@
 //  Created by Tomasz Kurylik
 //    - Twitter: https://twitter.com/tkurylik
 //    - Mail: tomasz.kurylik@mijick.com
+//    - GitHub: https://github.com/FulcrumOne
 //
 //  Copyright ©2023 Mijick. Licensed under MIT License.
 
