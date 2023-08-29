5da2bbd5db0f1a231971ed6e6a71c5506556e645
Tomasz Kurylik <fulcrumone@icloud.com>
2023-08-29 18:56:45 +0200

    1

--- Sources/Public/Extensions/Public+GridView.swift
+++ Sources/Public/Extensions/Public+GridView.swift
@@ -13,7 +13,7 @@ import SwiftUI
 
 // MARK: - Initialisers
 extension GridView {
-    public init<Data: RandomAccessCollection, ID: Hashable>(_ data: Data, id: KeyPath<Data.Element, ID>, @ViewBuilder content: @escaping (Data.Element) -> any View, configBuilder: (Config) -> Config) { self.init(
+    public init<Data: RandomAccessCollection, ID: Hashable>(_ data: Data, id: KeyPath<Data.Element, ID>, @ViewBuilder content: @escaping (Data.Element) -> any View, configBuilder: (Config) -> Config = { $0 }) { self.init(
         matrix: .init(configBuilder(.init())),
         elements: data.map { .init(content($0)) },
         config: configBuilder(.init())
